38fe1cdc19ce8e70a85d4d8db2fad2b88a041326
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-14 15:59:42 +0900

    [Feat] project 초기 세팅

---
+++ Picknic/Picknic/App/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Picknic
+//
+//  Created by Lee on 8/14/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
